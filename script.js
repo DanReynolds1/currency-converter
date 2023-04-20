875bc48a2b847aaa1a44529eb5240fb16749b546
DanReynolds1 <131292515+DanReynolds1@users.noreply.github.com>
2023-04-20 20:23:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 function convertCurrency() {
-    var urlParams = new URLSearchParams(window.location.search);
-    var amount = urlParams.get("amount");
-    var from_currency = urlParams.get("from_currency");
-    var to_currency = urlParams.get("to_currency");
+  // Get the values of the amount, from currency, and to currency fields from the web page.
+  const amount = document.getElementById("amount").value;
+  const fromCurrency = document.getElementById("from-currency").value;
+  const toCurrency = document.getElementById("to-currency").value;
 
-    fetch(`/convert?amount=${amount}&from_currency=${from_currency}&to_currency=${to_currency}`)
-        .then(response => response.text())
-        .then(data => {
-            var result = parseFloat(data).toFixed(2);
-            document.getElementById("converted-amount").innerHTML = result;
-        });
+  // Construct a URL that includes the values of the amount, from currency, and to currency fields as query parameters.
+  const url = `/convert?amount=${amount}&from_currency=${fromCurrency}&to_currency=${toCurrency}`;
+
+  // Send a GET request to the URL using the fetch() function in JavaScript.
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      // Parse the JSON response that is returned by the Flask API.
+      const rate = data.rates[toCurrency];
+      const convertedAmount = amount * rate;
+      const result = `${amount} ${fromCurrency} = ${convertedAmount.toFixed(2)} ${toCurrency}`;
+
+      // Update the web page with the converted amount.
+      document.getElementById("result").innerHTML = result;
+    })
+    .catch(error => console.log(error));
 }
